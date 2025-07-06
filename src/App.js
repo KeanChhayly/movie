7b4da7559117e07d940ce936c668e822a7d396a6
Kean Chhayly <Chhayly085@gmail.com>
2025-07-06 13:11:50 +0700

    update app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-// src/App.js
 import "./App.css";
 import "./index.css";
 import AllRoutes from "./routes/AllRoutes";
-
 import { Header, Footer } from "./components";
-// import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
-
 function App() {
   return (
     <div className="App">
